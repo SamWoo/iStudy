2a4d73b1b459605f368400884f681bedda6e8a77
SamWoo <765247593@qq.com>
2019-07-25 14:23:20 +0800

    create a new project in kotlin

---
+++ app/src/main/java/com/samwoo/istudy/activity/SplashActivity.kt
@@ -0,0 +1,64 @@
+package com.samwoo.istudy.activity
+
+import android.animation.AnimatorSet
+import android.os.Bundle
+import android.os.SystemClock
+import androidx.appcompat.app.AppCompatActivity
+import com.samwoo.istudy.R
+import kotlinx.android.synthetic.main.activity_splash.*
+import kotlin.random.Random
+import android.animation.ObjectAnimator
+import androidx.core.animation.addListener
+import org.jetbrains.anko.intentFor
+
+class SplashActivity : AppCompatActivity() {
+    private val SCALE_END: Float = 1.13f
+    private val ANIMATION_DURATION: Long = 2 * 1000
+    private val SPLASH_IMAGES = listOf<Int>(
+        R.drawable.splash0,
+        R.drawable.splash1,
+        R.drawable.splash2,
+        R.drawable.splash3,
+        R.drawable.splash4,
+
+        R.drawable.splash6,
+        R.drawable.splash7,
+        R.drawable.splash8,
+        R.drawable.splash9,
+        R.drawable.splash10,
+        R.drawable.splash11,
+        R.drawable.splash12,
+        R.drawable.splash13,
+        R.drawable.splash14,
+        R.drawable.splash15,
+        R.drawable.splash16
+    )
+    private lateinit var random: Random
+    private lateinit var animatorX: ObjectAnimator
+    private lateinit var animatorY: ObjectAnimator
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+
+        random = Random(SystemClock.elapsedRealtime())
+        iv_splash.setImageResource(SPLASH_IMAGES[random.nextInt(SPLASH_IMAGES.size)])
+        animateImage()
+    }
+
+    private fun animateImage() {
+        animatorX = ObjectAnimator.ofFloat(iv_splash, "scaleX", 1f, SCALE_END)
+        animatorY = ObjectAnimator.ofFloat(iv_splash, "scaleY", 1f, SCALE_END)
+        val animatorSet = AnimatorSet()
+        animatorSet.setDuration(ANIMATION_DURATION).play(animatorX).with(animatorY)
+        animatorSet.start()
+
+        animatorSet.addListener {
+            //            finish()
+            val intent = intentFor<MainActivity>()
+            startActivity(intent)
+            finish()
+        }
+
+    }
+}
