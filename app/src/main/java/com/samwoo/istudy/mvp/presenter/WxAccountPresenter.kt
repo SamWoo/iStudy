75cb8c7d01198263780a6fbaf275f25df30b8dc4
SamWoo <765247593@qq.com>
2019-08-19 17:18:15 +0800

    1.增加请求数据是显示加载动画； 2.添加自定义加载动画view； 3.修复一些已知的bug。

--- app/src/main/java/com/samwoo/istudy/mvp/presenter/WxAccountPresenter.kt
+++ app/src/main/java/com/samwoo/istudy/mvp/presenter/WxAccountPresenter.kt
@@ -6,6 +6,8 @@ import com.samwoo.istudy.bean.WxAccountBody
 import com.samwoo.istudy.callback.Callback
 import com.samwoo.istudy.mvp.contract.WxAccountContract
 import com.samwoo.istudy.mvp.model.WxAccountModel
+import org.jetbrains.anko.doAsync
+import org.jetbrains.anko.uiThread
 
 class WxAccountPresenter : BasePresenter<WxAccountContract.View>(), WxAccountContract.Presenter {
     private val wxAccountModel: WxAccountModel by lazy {
@@ -17,22 +19,28 @@ class WxAccountPresenter : BasePresenter<WxAccountContract.View>(), WxAccountCon
         wxAccountModel.getWxAccount(object : Callback<HttpResult<List<WxAccountBody>>, String> {
             override fun onSuccess(result: HttpResult<List<WxAccountBody>>) {
                 if (isViewAttached()) {
-                    mView?.run {
-                        setWxAccount(result.data)
+                    mView?.apply {
                         hideLoading()
+                        setWxAccount(result.data)
                     }
                 }
             }
 
             override fun onFail(msg: String) {
                 if (isViewAttached()) {
-                    mView?.apply {
-                        showError(msg)
-                        hideLoading()
+                    doAsync {
+                        //休眠2s模拟加载过程
+                        for (ratio in 0..10) Thread.sleep(200)
+                        ////处理完成，回到主线程在界面上显示
+                        uiThread {
+                            mView?.apply {
+                                hideLoading()
+                                showError(msg)
+                            }
+                        }
                     }
                 }
             }
         })
     }
-
 }
